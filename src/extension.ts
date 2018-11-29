5c92f2f8f4c2a398f3d2c1ce945141ccd606065f
nemi <neminovno@gmail.com>
2018-11-29 19:01:35 +0100

    Added newItemRenderer command

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,14 @@
 'use strict';
 
 import * as vscode from 'vscode';
-import NewProjectCommand from './project-builder/NewProject';
+import NewItemRendererCommand from './item-renderer-builder/NewItemRenderer';
 import { log_dispose } from './logger';
+import NewProjectCommand from './project-builder/NewProject';
 
 export function activate(context: vscode.ExtensionContext) {
 
     const newProjectCommand = new NewProjectCommand();
+    const newItemRendererCommand = new NewItemRendererCommand();
 
     let disposable = vscode.commands.registerCommand('extension.newProject', () => {
 
@@ -24,8 +26,17 @@ export function activate(context: vscode.ExtensionContext) {
         newProjectCommand.start(openedFolders, context.extensionPath, sdkPath);
     });
 
+    let disposable_newItemRenderer = vscode.commands.registerCommand('extension.newItemRenderer', async (args) => {
+        //console.log('newItemRenderer args: ' + args);
+        //console.log('newItemRenderer args: ' + args.fsPath);
+
+        newItemRendererCommand.start(context.extensionPath, args.fsPath);
+    });
+
     context.subscriptions.push(newProjectCommand);
+    context.subscriptions.push(newItemRendererCommand);
     context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable_newItemRenderer);
 }
 
 export function deactivate() {
